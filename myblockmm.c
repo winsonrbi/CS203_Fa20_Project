86c05b13d2fc3e52e0388e16f0ebe39b405b297b
Winson Bi <wbi002@ti-05.cs.ucr.edu>
2020-12-07 11:51:06 -0800

    Fixed issue with array size being less than 256

--- myblockmm.c
+++ myblockmm.c
@@ -80,8 +80,15 @@ void *mythreaded_vector_blockmm(void *t)
   double **b = tinfo.b;
   double **c = tinfo.c;
   int ARRAY_SIZE = tinfo.array_size;
+  int n;
   //Optimization 3: Calculate optimal block size instead of using input
-  int n = ARRAY_SIZE/64;
+  if (ARRAY_SIZE >= 256)
+  {
+    n = ARRAY_SIZE/64;
+  }
+  else{
+   n = ARRAY_SIZE/8; 
+  }
   // i is for :i
   for(i = (ARRAY_SIZE/number_of_threads)*(tid); i < (ARRAY_SIZE/number_of_threads)*(tid+1); i+=ARRAY_SIZE/n)
   {
